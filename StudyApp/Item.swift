c9e083bd5c5e31c1460938bfcd901a07eb82e8e9
Rajesh Mani <mobdevrajesh@gmail.com>
2024-09-21 18:56:56 +0400

    Initial Commit

---
+++ StudyApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  StudyApp
+//
+//  Created by Rajesh Mani on 21/09/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
