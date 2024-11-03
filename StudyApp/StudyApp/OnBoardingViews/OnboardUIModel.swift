711f16bd4f1236c687fdf394764d9bf159b60f54
Rajesh Mani <rajeshm79@outlook.com>
2024-11-03 13:04:54 +0530

    added project folder with files

---
+++ StudyApp/StudyApp/OnBoardingViews/OnboardUIModel.swift
@@ -0,0 +1,14 @@
+//
+//  OnboardUIModel.swift
+//  StudyApp
+//
+//  Created by Rajesh Mani on 29/09/24.
+//
+
+import Foundation
+// Data Model for Each Page
+struct PageData {
+    var imageName: String
+    var title: String
+    var description: String
+}
---
+++ StudyApp/StudyAppTests/StudyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  StudyAppTests.swift
+//  StudyAppTests
+//
+//  Created by Rajesh Mani on 21/09/24.
+//
+
+import Testing
+@testable import StudyApp
+
+struct StudyAppTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
